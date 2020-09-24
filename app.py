d3cb7a90886b4b4b540509f0256b3c87dffbf7da
titaevskaya <titaevskaya@gmail.com>
2020-09-25 02:22:05 +0300

    project commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template  # сперва подключим модуль
+import json
+from itertools import islice
+
+app = Flask(__name__)  # объявим экземпляр фласка
+
+with open("tours.json", encoding='utf-8') as file:
+    tours = json.load(file)
+
+
+@app.route('/')
+def render_main():
+    return render_template('index.html', tours=dict(islice(tours["tours"].items(), 6)), main_pic=tours["tours"]['9'],
+                           tours_all_field=tours)
+
+
+@app.route('/departures/<departure>/')
+def render_departures(departure):
+    filtered_tours = [v for k, v in tours['tours'].items() if v.get('departure') == departure]
+    return render_template('departure.html', tours=filtered_tours,
+                           departures=tours["departures"], departure=departure)
+
+
+@app.route('/tours/<id>/')
+def render_tours(id):
+    return render_template('tour.html', tour=tours["tours"][id], departures=tours["departures"])
+
+
+
+app.run()  # запустим сервер
